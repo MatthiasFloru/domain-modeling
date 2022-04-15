9c03d11880aece4c9350ae42cbcefd2efd9e1cef
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-04-15 21:25:43 +0000

    finished in class domain modeling practice

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,44 @@
+-- Drops existing tables, so we start fresh with each
+-- run of this script
+-- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
 
+
+
+-- CREATE TABLES
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT,
+  real_name TEXT,
+  location TEXT
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  image_file TEXT,
+  created_at TEXT,
+  user_id INTEGER
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER,
+  body TEXT
+);
+
+CREATE TABLE followers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  follower_user_id INTEGER,
+  followed_user_id INTEGER
+);
